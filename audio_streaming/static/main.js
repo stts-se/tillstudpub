ae733dfd985ba65c735b5f0c3f6e06bac9538f0d
Hanna Lindgren <hanna@stts.se>
2020-04-27 18:34:54 +0200

    Fix #1: initialize audio only once

--- audio_streaming/static/main.js
+++ audio_streaming/static/main.js
@@ -12,8 +12,8 @@ const keyCodeEscape = 27;
 let mediaAccess;
 let recorder;
 let micDetected = true;
-let speechDetected = false;
 
+let context;
 var channelCount = 1;
 
 let bytesSent = 0;
@@ -38,31 +38,31 @@ function addStyle(element, theStyle) {
 function removeStyle(element, theStyle) {
     let allS = element.getAttribute("style");
     if (nonEmptyString(allS)) {
-	let newSs = [];
-	let allSs = allS.split(/ *; +/);
-	for (let i=0; i<allSs.length;i++) {
-	    const thisS = allSs[i];
-	    let key = thisS.split(/ *: */)[0].trim();
-	    if (key !== theStyle) {
-		newSs.push(thisS);
-	    }
-	}
-	element.setAttribute("style", newSs.join(" "));
+        let newSs = [];
+        let allSs = allS.split(/ *; +/);
+        for (let i = 0; i < allSs.length; i++) {
+            const thisS = allSs[i];
+            let key = thisS.split(/ *: */)[0].trim();
+            if (key !== theStyle) {
+                newSs.push(thisS);
+            }
+        }
+        element.setAttribute("style", newSs.join(" "));
     }
 }
 
 function removeClass(element, theClass) {
     let allC = element.getAttribute("class");
     if (nonEmptyString(allC)) {
-	let newCs = [];
-	let allCs = allC.split(/ +/);
-	for (let i=0; i<allCs.length;i++) {
-	    const thisC = allCs[i];
-	    if (thisC !== theClass) {
-		newCs.push(thisC);
-	    }
-	}
-	element.setAttribute("class", newCs.join(" "));
+        let newCs = [];
+        let allCs = allC.split(/ +/);
+        for (let i = 0; i < allCs.length; i++) {
+            const thisC = allCs[i];
+            if (thisC !== theClass) {
+                newCs.push(thisC);
+            }
+        }
+        element.setAttribute("class", newCs.join(" "));
     }
 }
 
@@ -72,12 +72,12 @@ function nonEmptyString(s) {
 
 function logMessage(title, text, stacktrace) {
     if (stacktrace !== undefined) {
-	//const stack = new Error().stack;
-	console.log("logMessage", title, text, stacktrace.stack);
-    } else {	
-	console.log("logMessage", title, text);
+        //const stack = new Error().stack;
+        console.log("logMessage", title, text, stacktrace.stack);
+    } else {
+        console.log("logMessage", title, text);
     }
-    document.getElementById("messages").textContent = title + ": " + text;    
+    document.getElementById("messages").textContent = title + ": " + text;
 }
 
 function sleep(ms) {
@@ -94,13 +94,13 @@ function loadUserSettings() { // TEMPLATE
     // TODO: Save settings between sessions
     let urlParams = new URLSearchParams(window.location.search);
     if (urlParams.has('project')) {
-    	projectname.value=urlParams.get("project");
+        projectname.value = urlParams.get("project");
     }
     if (urlParams.has('session')) {
-    	sessionname.value=urlParams.get("session");
+        sessionname.value = urlParams.get("session");
     }
     if (urlParams.has('user')) {
-    	username.value=urlParams.get("user");
+        username.value = urlParams.get("user");
     }
 }
 
@@ -114,123 +114,129 @@ function disableAll() {
     document.getElementById("recstart").disabled = true;
 }
 
-document.getElementById("recstart").addEventListener("click", function() {
+document.getElementById("recstart").addEventListener("click", function () {
     //currentBlob = null;
 
     let wsURL = "ws://" + baseURL + "/ws/register";
     console.log(wsURL);
     audioWS = new WebSocket(wsURL);
 
-    let context;
+    audioWS.onopen = function () {
+        console.log("websocket opened");
 
-    audioWS.onopen = function() {
-	console.log("websocket opened");
+        let handshake = {
+            'label': 'handshake',
+            'handshake': {
+                'sample_rate': context.sampleRate,
+                'channel_count': channelCount,
+                'encoding': defaultEncoding(),
+                //'user': user, ... etc
+                'user_agent': navigator.userAgent,
+                'timestamp': new Date().toISOString(),
+                'user_name': username.value,
+                'project': project.value,
+                'session': session.value,
+            },
+        };
+        console.log("sending handshake to server", handshake);
+        audioWS.send(JSON.stringify(handshake));
+    }
+    audioWS.onerror = function () {
+        logMessage("error", "Couldn't open data socket");
+    }
+    audioWS.onmessage = function (evt) { // TODO: messages should be sent over a separate message channel
+        let resp = JSON.parse(evt.data);
+        if (resp["label"] === "handshake") {
+            console.log("received streaming handshake from server", resp);
+            if (resp["error"] != undefined && resp["error"] != "") {
+                logMessage("error", "received error from server handshake: " + resp["error"]);
+                recStop();
+            }
+            else {
+                //startStreaming(context);
+                document.getElementById("recstop").disabled = false;
+                document.getElementById("recstart").disabled = true;
+                document.getElementById("audiofeedbacktextspan").innerText = "";
+                document.getElementById("bigmic").src = bigMicOnSrc;
 
-	let audioContext = window.AudioContext || window.webkitAudioContext;
-	context = new audioContext();
-	
-	let handshake = {
-	    'label': 'handshake',
-	    'handshake': {
-		'sample_rate': context.sampleRate,
-		'channel_count': channelCount,
-		'encoding': defaultEncoding(),
-		//'user': user, ... etc
-		'user_agent' : navigator.userAgent,
-		'timestamp': new Date().toISOString(),
-		'user_name': username.value,
-		'project': project.value,
-		'session': session.value,
-	    },
-	};
-	console.log("sending handshake to server", handshake);
-	audioWS.send(JSON.stringify(handshake));
+                //logMessage("info", "recording started");
+                console.log("recording started");
+            }
+        }
     }
-    audioWS.onerror = function() {
-	logMessage("error","Couldn't open data socket");
+    audioWS.onclose = function () {
+        console.log("websocket closed");
     }
-    audioWS.onmessage = function(evt) { // TODO: messages should be sent over a separate message channel
-	let resp = JSON.parse(evt.data);
-	if (resp["label"] === "handshake") {
-		console.log("received streaming handshake from server", resp);
-	    if (resp["error"] != undefined && resp["error"] != "") {
-		logMessage("error", "received error from server handshake: " + resp["error"]);
-		recStop();
-	    }
-	    else {
-		startStreaming(context);
-	    }
-	}
-    }
-    audioWS.onclose = function() {
-	console.log("websocket closed");
-    }
-    
-    document.getElementById("audiofeedbacktextspan").innerText = "";
-    document.getElementById("bigmic").src = bigMicOnSrc;
 
-    //logMessage("info", "recording started");
-    console.log("recording started");
 
-    document.getElementById("recstop").disabled = false;
-    document.getElementById("recstart").disabled = true;
+
     // recorder.start();
 });
 
-function startStreaming(context) {
-    //console.log("supported constraints", navigator.mediaDevices.getSupportedConstraints());
-    let constraints = {audio: true};
 
-    navigator.mediaDevices.getUserMedia(constraints)
-    // on success:
-	.then(function(stream) {
+function initStreamer() {
+    if (!navigator.mediaDevices.getUserMedia)
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-	    VISUALISER.init(isRecording);		
-	    VISUALISER.connect(stream);
-	    
-	    let audioInput = context.createMediaStreamSource(stream);
-	    var bufferSize = 1024;
-	    recorder = context.createScriptProcessor(bufferSize, channelCount, channelCount);
-	    audioInput.connect(recorder)
-	    recorder.connect(context.destination);
-	    
-	    recorder.onaudioprocess = function(e){
-		//console.log("recorder.onaudioprocess");
-		if(!isRecording()) return;
-		//logMessage("info", "recording");
-		var left = e.inputBuffer.getChannelData(0);
-		let sendable = convertFloat32ToInt16(left);
-		bytesSent = bytesSent + sendable.byteLength;
-		//console.log("streaming " + sendable.byteLength + " bytes of audio");
-		audioWS.send(sendable);
-	    }
+    if (!navigator.mediaDevices.getUserMedia) {
+        disableEverything();
+        alert('getUserMedia not supported in this browser.');
+    }
 
-	})
 
-    // on error:
-	.catch(function(err) {
-    	    console.log("error from getUserMedia", err);
-    	    micDetected = false;
-    	    logMessage("error", "No microphone detected. Please verify that your microphone is properly connected.");
-    	    alert("Couldn't initialize recorder: " + err.message + "\n\nPlease verify that your microphone is properly connected.");
-	});
+    let audioCtx = window.AudioContext || window.webkitAudioContext;
+    context = new audioCtx();
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        // on success:
+        .then(function (stream) {
+
+            VISUALISER.init(isRecording);
+            VISUALISER.connect(stream);
+
+            let audioInput = context.createMediaStreamSource(stream);
+            var bufferSize = 1024;
+            recorder = context.createScriptProcessor(bufferSize, channelCount, channelCount);
+            audioInput.connect(recorder)
+            recorder.connect(context.destination);
+
+            recorder.onaudioprocess = function (e) {
+                //console.log("recorder.onaudioprocess");
+                if (!isRecording()) return;
+                //logMessage("info", "recording");
+                var left = e.inputBuffer.getChannelData(0);
+                let sendable = convertFloat32ToInt16(left);
+                bytesSent = bytesSent + sendable.byteLength;
+                //console.log("streaming " + sendable.byteLength + " bytes of audio");
+                audioWS.send(sendable);
+            }
+
+        })
+
+        // on error:
+        .catch(function (err) {
+            console.log("error from getUserMedia", err);
+            micDetected = false;
+            logMessage("error", "No microphone detected. Please verify that your microphone is properly connected.");
+            alert("Couldn't initialize recorder: " + err.message + "\n\nPlease verify that your microphone is properly connected.");
+        });
 
 }
 
-document.getElementById("recstop").addEventListener("click", function() {
+document.getElementById("recstop").addEventListener("click", function () {
     recStop();
 });
-    
-function recStop() {						    
+
+function recStop() {
     if (recorder === null) {
-	msg = "Cannot record -- recorder is undefined";
-	console.log(msg);
-	alert(msg);
+        msg = "Cannot record -- recorder is undefined";
+        console.log(msg);
+        alert(msg);
     }
     //logMessage("info", "recording stopped");
     console.log("recording stopped");
     console.log("sent " + bytesSent + " bytes in total");
-    bytesSent=0;
+    bytesSent = 0;
 
     document.getElementById("recstop").disabled = true;
     document.getElementById("recstart").disabled = false;
@@ -247,7 +253,7 @@ function convertFloat32ToInt16(buffer) {
     var buf = new Int16Array(l)
 
     while (l--) {
-	buf[l] = buffer[l]*0xFFFF; //convert to 16 bit
+        buf[l] = buffer[l] * 0xFFFF; //convert to 16 bit
     }
     return buf.buffer
 }
@@ -256,43 +262,32 @@ function convertFloat32ToInt16(buffer) {
 function defaultEncoding() {
     let browser = navigator.userAgent;
     if (browser.toLowerCase().indexOf("chrome") != -1) {
-	return "flac";
+        return "flac";
     } else if (browser.indexOf("mozilla") != -1 || browser.indexOf("firefox") != -1) {
-	return "pcm";
+        return "pcm";
     }
     return "pcm";
 }
 
 window.onload = async function () {
-    
-    loadUserSettings();
-    initSettings();
 
-    VISUALISER.init(isRecording);
-
-    if (!navigator.mediaDevices.getUserMedia)
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-    if (!navigator.mediaDevices.getUserMedia) {
-	disableEverything();
-	alert('getUserMedia not supported in this browser.');
-    }
-
+    this.loadUserSettings();
+    this.initSettings();
+    this.initStreamer();
 }
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     if (isRecording()) {
-	return "Are you sure you want to navigate away?";
+        return "Are you sure you want to navigate away?";
     }
 }
 
-window.onunload = function() {
+window.onunload = function () {
     // console.log("window.onunload");
     // TODO: can we do something here? close the session?
 }
 
-document.addEventListener("keydown", function(evt) { // TEMPLATE FOR KEYBOARD SHORTCUTS
+document.addEventListener("keydown", function (evt) { // TEMPLATE FOR KEYBOARD SHORTCUTS
     //console.log("window.keydown", evt.keyCode);
     // if (evt.keyCode === keyCodeSpace) {
     // }
