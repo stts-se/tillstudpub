b0d8cc6739d358720259bb614fbd7a17895174c3
Hanna Lindgren <han.lind@gmail.com>
2020-04-29 14:23:39 +0200

    Fixed bug with javascript variables shared over different files

--- audio_streaming/static/main.js
+++ audio_streaming/static/main.js
@@ -85,8 +85,127 @@ function logMessage(title, text, stacktrace) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
 // END: UTIL
+
+
+function convertFloat32ToInt16(buffer) {
+    var l = buffer.length;
+    var buf = new Int16Array(l)
+
+    while (l--) {
+        buf[l] = buffer[l] * 0xFFFF; //convert to 16 bit
+    }
+    return buf.buffer
+}
+
+function initStreamerWithScriptProcessor() {
+    console.log("initStreamerWithScriptProcessor called");
+    if (!navigator.mediaDevices.getUserMedia)
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    if (!navigator.mediaDevices.getUserMedia) {
+        disableEverything();
+        alert('getUserMedia not supported in this browser.');
+        return false;
+    }
+
+
+    let audioCtx = window.AudioContext || window.webkitAudioContext;
+    context = new audioCtx();
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        // on success:
+        .then(function (stream) {
+            VISUALISER.init(isRecording);
+            VISUALISER.connect(stream);
+
+            let audioInput = context.createMediaStreamSource(stream);
+            var bufferSize = 1024;
+            recorder = context.createScriptProcessor(bufferSize, channelCount, channelCount);
+            audioInput.connect(recorder)
+            recorder.connect(context.destination);
+
+            recorder.onaudioprocess = function (e) {
+                if (!isRecording()) return;
+                var left = e.inputBuffer.getChannelData(0);
+                let sendable = convertFloat32ToInt16(left);
+                bytesSent = bytesSent + sendable.byteLength;
+                audioWS.send(sendable);
+            }
+            return true;
+        })
+
+        // on error:
+        .catch(function (err) {
+            console.log("error from getUserMedia", err);
+            micDetected = false;
+            logMessage("error", "No microphone detected. Please verify that your microphone is properly connected.");
+            alert("Couldn't initialize recorder: " + err.message + "\n\nPlease verify that your microphone is properly connected.");
+            disableEverything();
+            return false;
+        });
+}
+
+function initStreamerWithAudioWorklet() {
+    console.log("initStreamerWithAudioWorklet called");
+    if (!navigator.mediaDevices.getUserMedia)
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    if (!navigator.mediaDevices.getUserMedia) {
+        disableEverything();
+        alert('getUserMedia not supported in this browser.');
+        return false;
+    }
+
+
+    let audioCtx = window.AudioContext || window.webkitAudioContext;
+    context = new audioCtx();
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        // on success:
+        .then(async function (stream) {
+            VISUALISER.init(isRecording);
+            VISUALISER.connect(stream);
+
+            let audioSource = context.createMediaStreamSource(stream);
+            await context.audioWorklet.addModule('processor.js');
+            const recorder = new AudioWorkletNode(context, 'recorder-worklet');
+            audioSource.connect(recorder).connect(context.destination);
+
+            recorder.port.onmessage = function (e) {
+                if (e.data.eventType === 'data') {
+                    console.log("recorder.ondata");
+                    const audioData = e.data.audioBuffer;
+                    if (!isRecording()) return;
+                    var left = e.inputBuffer.getChannelData(0);
+                    let sendable = convertFloat32ToInt16(left);
+                    bytesSent = bytesSent + sendable.byteLength;
+                    audioWS.send(sendable);
+                }
+                if (e.data.eventType === 'stop') {
+                    // recording has stopped
+                }
+            };
+            //let time = new Date().getTime(); // ??? 
+            //let duration = 1; // ??? 
+            //recorder.parameters.get('isRecording').setValueAtTime(1, time);
+            //recorder.parameters.get('isRecording').setValueAtTime(0, time + duration);
+            //yourSourceNode.start(time); // ??? 
+            return true;
+        })
+
+        // on error:
+        .catch(function (err) {
+            console.log("error from getUserMedia", err);
+            micDetected = false;
+            logMessage("error", "No microphone detected. Please verify that your microphone is properly connected.");
+            alert("Couldn't initialize recorder: " + err.message + "\n\nPlease verify that your microphone is properly connected.");
+            disableEverything();
+            return false;
+        });
+
+}
+
 
 function isRecording() {
     return document.getElementById("recstop").disabled === false;
