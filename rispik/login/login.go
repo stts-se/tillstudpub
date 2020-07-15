02ef5ec44b6fbd4990aa9a1bc5cd127ff09b109f
Hanna Lindgren <han.lind@gmail.com>
2020-07-15 18:32:48 +0200

    login/login.go godoc

--- rispik/login/login.go
+++ rispik/login/login.go
@@ -4,16 +4,19 @@ import (
 	"time"
 )
 
+// Users is a hardwired list of available users
 var Users = []string{
 	"gäst",
 }
 
+// Projects is a hardwired list of available projects
 var Projects = []string{
 	"tillstud_demo",
 }
 
 var timestamp = time.Now().Format("2006-01-02 15:04")
 
+// Sessions is a hardwired list of available sessions
 var Sessions = []string{
 	"demo_session",
 	timestamp,
